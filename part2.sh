d8662712220309112f08c60d858c7abe82ccd0e8
tdh1g19 <tdh1g19@soton.ac.uk>
2022-03-09 12:54:46 +0000

    part 2 done???

--- part2.sh
+++ part2.sh
@@ -3,6 +3,7 @@ tpm2_clear
 tpm2_startup -c
 tpm2_changeauth -c owner password
 
+# create a symmetric key for encryption and decryption
 tpm2_createprimary -C e -c primary.ctx
 tpm2_create -G aes128 -u aes.pub -r aes.priv -C primary.ctx
 
@@ -10,30 +11,40 @@ tpm2_flushcontext -t
 
 tpm2_load -C primary.ctx -u aes.pub -r aes.priv -c aes.ctx
 
-tpm2_encryptdecrypt -c aes.ctx -o dummy_enc1.txt dummy.txt
+# encrypt the secret file, remove original if practical
+tpm2_encryptdecrypt -c aes.ctx -o dummy_enc.txt dummy.txt
+#rm dummy.txt
 
 tpm2_flushcontext -t
 
-tpm2_pcrread -o pcr.dat "sha1:0,1,2,3"
+# setup a policy
+tpm2_pcrread -o pcr.dat sha256:0,1,2,3
 tpm2_startauthsession -S session.ctx
-tpm2_policypcr -S session.ctx -l "sha1:0,1,2,3" -f pcr.dat -L policy1.dat
+tpm2_policypcr -S session.ctx -l sha256:0,1,2,3 -f pcr.dat -L policy1.dat
 
 tpm2_flushcontext session.ctx
+rm session.ctx
 
-tpm2_create -Q -u key.pub -r key.priv -C primary.ctx -L policy1.dat -i- <<< dummy_enc1.txt
+# create a key from the policy and store the secret file
+tpm2_create -u key.pub -r key.priv -C primary.ctx -L policy1.dat -i- <<< aes.ctx
+rm aes.ctx
 
 tpm2_flushcontext -t
 
 tpm2_load -C primary.ctx -u key.pub -r key.priv -n unseal.key.name -c unseal.key.ctx
 
-tpm2_startauthsession --policy-session -S session.dat
-tpm2_policypcr -S session.dat -l "sha1:0,1,2,3" -f pcr.dat -L policy.dat
-tpm2_unseal -p session:session.dat -c unseal.key.ctx -o dummy_enc2.txt
+# use the policy key to get the encrypted file back
+tpm2_startauthsession --policy-session -S session.ctx
+tpm2_policypcr -S session.ctx -l sha256:0,1,2,3 -f pcr.dat -L policy.dat
+tpm2_unseal -p session:session.ctx -c unseal.key.ctx -o aes.ctx
 
 tpm2_flushcontext -t
 
+# decrypt the secret file
 tpm2_load -C primary.ctx -u aes.pub -r aes.priv -c aes.ctx
-tpm2_encryptdecrypt -d -c aes.ctx -o dummy_enc2.txt dummy2.txt
+tpm2_encryptdecrypt -d -c aes.ctx -o dummy2.txt dummy_enc.txt
 cat dummy2.txt
+rm dummy_enc.txt
 
-tpm2_flushcontext session.dat
+tpm2_flushcontext session.ctx
+rm session.ctx
